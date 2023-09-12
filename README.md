5363ec0feb50dde9da281525a712d15c449bac3b
muhammad.bilal1 <muhammad.bilal1@axabiztech.com>
2023-09-12 12:59:45 +0500

    readme updates

--- README.md
+++ README.md
@@ -41,5 +41,24 @@ allprojects {
 dependencies {
     implementation 'com.github.Bilal512:SimbaAds:Tag'
 }
-```
+````
+
+2. In Your Application class
+````
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        SimbaAds
+            .builder()
+            .setIsDebug(BuildConfig.DEBUG)
+            .setTestDeviceId("9257EDC2AC708B3D76BD11D96DEA6FD4")
+            .setAdmobAppOpenId("")
+            .setTestDeviceId("")
+            .setAdNetwork(AdNetwork.ADMOB)
+            .build(this)
+    }
+}
+````
 
--- app/src/main/java/com/simbaone/App.kt
+++ app/src/main/java/com/simbaone/App.kt
@@ -2,6 +2,7 @@ package com.simbaone
 
 import android.app.Application
 import com.simbaone.adslibrary.BuildConfig
+import com.simbaone.simba_ads.AdNetwork
 
 class App: Application() {
 
@@ -10,8 +11,11 @@ class App: Application() {
 
         SimbaAds
             .builder()
-            .setTestDeviceId("9257EDC2AC708B3D76BD11D96DEA6FD4")
             .setIsDebug(BuildConfig.DEBUG)
+            .setTestDeviceId("9257EDC2AC708B3D76BD11D96DEA6FD4")
+            .setAdmobAppOpenId("")
+            .setTestDeviceId("")
+            .setAdNetwork(AdNetwork.ADMOB)
             .build(this)
     }
 }
