d189c89aa84954b92d117cad6ea4bc4e1aa47bbc
muhammad.bilal1 <muhammad.bilal1@axabiztech.com>
2023-09-13 12:41:23 +0500

    slight changes of indentations

--- simba_ads/build.gradle.kts
+++ simba_ads/build.gradle.kts
@@ -1,7 +1,17 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
+
+val githubProperties = Properties()
+githubProperties.load(FileInputStream(rootProject.file("github.properties")))
+
+val versionName get() = "1.0.0"
+val getArtificatId = "SimbaAds"
 
 android {
     namespace = "com.simbaone.simba_ads"
@@ -33,6 +43,35 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    afterEvaluate {
+        publishing {
+            publications {
+                create<MavenPublication>("bar") {
+                    groupId = namespace // Replace with group ID
+                    artifactId = getArtificatId
+                    version = versionName
+                    artifact("$buildDir/outputs/aar/${getArtificatId}-release.aar")
+                }
+            }
+
+            repositories {
+                maven {
+                    name = "GitHubPackages"
+                    /** Configure path of your package repository on Github
+                     *  Replace GITHUB_USERID with your/organisation Github userID and REPOSITORY with the repository name on GitHub
+                     */
+                    url = uri("https://maven.pkg.github.com/Bilal512/SimbaAds")
+
+                    credentials {
+                        /**Create github.properties in root project folder file with gpr.usr=GITHUB_USER_ID  & gpr.key=PERSONAL_ACCESS_TOKEN**/
+                        username = (githubProperties["gpr.usr"] ?: System.getenv("GPR_USER")).toString()
+                        password = (githubProperties["gpr.key"] ?: System.getenv("GPR_API_KEY")).toString()
+                    }
+                }
+            }
+        }
+    }
 }
 
 dependencies {
@@ -44,8 +83,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    api ("com.facebook.android:audience-network-sdk:6.15.0")
-    api ("com.google.android.gms:play-services-ads:22.3.0")
-    implementation ("com.facebook.shimmer:shimmer:0.5.0")
-    implementation ("com.intuit.sdp:sdp-android:1.1.0")
+    api("com.facebook.android:audience-network-sdk:6.15.0")
+    api("com.google.android.gms:play-services-ads:22.3.0")
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
 }
